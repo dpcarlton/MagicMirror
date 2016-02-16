8294c46e426306fef5a189029d2c172783966c2f
dpcarlton <dpcarlton@gmail.com>
2016-02-16 14:02:29 -0700

    Update weather.js

--- js/weather/weather.js
+++ js/weather/weather.js
@@ -21,11 +21,12 @@ var weather = {
                 'wind':'wi-night-alt-cloudy-windy'
         },
         temperatureLocation: '.temp',
+        windSunLocation: '.windsun',
         forecastLocation: '.forecast',
         urlBase: 'https://api.forecast.io/forecast/',
         urlEnd:  '?exclude=minutely,flags,hourly,alerts',
-        updateInterval: config.weather.interval || 90000,
-        fadeInterval: config.weather.fadeInterval || 1000,
+        updateInterval: config.weather.interval || 300000,
+        fadeInterval: config.weather.fadeInterval || 100000,
         intervalId: null,
         orientation: config.weather.orientation || 'vertical',
 }
@@ -46,11 +47,12 @@ weather.roundValue = function (temperature) {
  * @return {int}     The wind speed converted into its corresponding Beaufort number
  */
 weather.ms2Beaufort = function(ms) {
-        var kmh = ms * 60 * 60 / 1000;
-        var speeds = [1, 5, 11, 19, 28, 38, 49, 61, 74, 88, 102, 117, 1000];
+        //modified for mph > knots, and correct the scale.
+        var knots = parseFloat(ms *0.868976).toFixed(0);
+        var speeds = [0,1, 4, 7, 11, 17, 22, 28, 34, 41, 48, 56, 61];
         for (var beaufort in speeds) {
                 var speed = speeds[beaufort];
-                if (speed > kmh) {
+                if (speed > knots) {
                         return beaufort;
                 }
         }
@@ -64,8 +66,14 @@ weather.ms2Beaufort = function(ms) {
 weather.updateCurrentWeather = function () {
         $.ajax({
                 type: 'GET',
-                crossDomain: true,
-                url: weather.urlBase + weather.params.apiID + '/' + weather.params.loc + weather.urlEnd + '&' + weather.params.units + '&' + weather.params.lang,
+                dataType: 'json',
+
+                //**
+                  * add unix format timestamp to the end of the URL. forecast.io is setting the http expire header, which means the data is never
+                  * refreshed in in the browser. It ignore this string, but it appears to the browser it is a new un-expired request each time.
+                  */
+
+                url: weather.urlBase + weather.params.apiID + '/' + weather.params.loc + weather.urlEnd + '&' + weather.params.units + '&' + weather.params.lang + '&' + moment().format('x'),
                 success: function (data) {
 
                         var _temperature = this.roundValue(data.currently.temperature),
@@ -73,6 +81,7 @@ weather.updateCurrentWeather = function () {
                                 _temperatureMax = this.roundValue(data.daily.data[0].temperatureMax),
                                 _wind = this.roundValue(data.currently.windSpeed),
                                 _iconClass = this.iconTable[data.currently.icon];
+
 
                         var _icon = '<span class="icon ' + _iconClass + ' dimmed wi"></span>';
 
@@ -84,10 +93,13 @@ weather.updateCurrentWeather = function () {
                                 _sunrise = moment(data.daily.data[0].sunriseTime*1000).format('HH:mm'),
                                 _sunset = moment(data.daily.data[0].sunsetTime*1000).format('HH:mm');
 
+
                         var _newWindHtml = '<span class="wind"><span class="wi wi-strong-wind xdimmed"></span> ' + this.ms2Beaufort(_wind) + '</span>',
+
                                 _newSunHtml = '<span class="sun"><span class="wi wi-sunrise xdimmed"></span> ' + _sunrise + '</span>';
 
                         if (_sunrise < _now && _sunset > _now) {
+
                                 _newSunHtml = '<span class="sun"><span class="wi wi-sunset xdimmed"></span> ' + _sunset + '</span>';
                         }
 
@@ -105,12 +117,16 @@ weather.updateCurrentWeather = function () {
  * Updates the 5 Day Forecast from the api.forecast.io
  */
 weather.updateWeatherForecast = function () {
-
         $.ajax({
-                type: 'GET',
-                crossDomain: true,
-                url: weather.urlBase + weather.params.apiID + '/' + weather.params.loc + weather.urlEnd + '&' + weather.params.units + '&' + weather.params.lang,
 
+                //**
+                  * add unix format timestamp to the end of the URL. forecast.io is setting the http expire header, which means the data is never
+                  * refreshed in in the browser. It ignore this string, but it appears to the browser it is a new un-expired request each time.
+                  */
+
+                type: 'GET',
+                url: weather.urlBase + weather.params.apiID + '/' + weather.params.loc + weather.urlEnd + '&' + weather.params.units + '&' + weather.params.lang + '&' + moment().format('x'),
+                dataType: 'json',
                 success: function (data) {
 
                         var _opacity = 1,
@@ -172,6 +188,8 @@ weather.init = function () {
                 this.updateCurrentWeather();
                 this.updateWeatherForecast();
         }.bind(this), this.updateInterval);
+
         this.updateCurrentWeather();
+
         this.updateWeatherForecast();
 }
